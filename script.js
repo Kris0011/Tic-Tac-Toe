7eba36f92f08cd5219dffe2b81b196b6b10fbe96
Kris Patel <krishp759@gmail.com>
2023-05-31 21:22:01 +0530

    changed js

--- script.js
+++ script.js
@@ -4,20 +4,32 @@ let currentPlayer = "X" ;
 let board = [" "," "," "," "," "," "," "," "," "," "];
 let winner = null ;
 
-
-
+var i = 0;
+var flag = 0;
 function move(index) {
-  if (gameActive   && ( board[index] === " ")) {
+  if (gameActive && board[index] === " ") {
     board[index] = currentPlayer;
-    
+    document.getElementById(`cell${index}`).innerHTML = "X";
+    if (currentPlayer == "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
   }
+  
+    if(i==0)
+    {
+      console.log('Yes');
+        if(board[4]==" ")
+        {
+            board[4]="O";
+            document.getElementById(`cell4`).innerHTML = "O";
+        }
+    
+        
+    }
 }
-console.log(board);
-
-
-while(winner == null){
 
 
 
 
-}
